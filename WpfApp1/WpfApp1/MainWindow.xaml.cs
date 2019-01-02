f3efc8d0e7d0246cd64c10b1193ab62169cd5da2
GRuned <46294382+GRuned@users.noreply.github.com>
2019-01-02 21:43:56 +0700

    Project 1

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            days.Text = ""+ int.Parse(want.Text) / (int.Parse(income.Text) - int.Parse(expend.Text));
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Want_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Days_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Income_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
